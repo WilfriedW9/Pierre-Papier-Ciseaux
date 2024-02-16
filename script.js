e341d1766631a15722ef1264e7ec6eb043d051f9
WilfriedW9 <zalo.Timothe@gmail.com>
2024-02-16 16:14:49 +0100

    Fonctionnalitées basiques

---
+++ script.js
@@ -0,0 +1,57 @@
+const player = document.querySelector("#player");
+const buttons = document.querySelectorAll("button");
+const options = ["Pierre", "Papier", "Ciseaux"];
+const opponent = document.querySelector("#opponent");
+const counter = document.querySelector("#counter");
+const startBtn = document.querySelector("#startBtn");
+const time = 2500
+
+let count = 3;
+console.log(buttons);
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const btnValue = button.attributes.value.value;
+    player.innerText = btnValue;
+    console.log(btnValue);
+    return btnValue;
+  });
+});
+
+function oppChoices() {
+  const oppChoice = options[Math.floor(Math.random() * 3)];
+
+  setTimeout(() => {
+    opponent.innerText = oppChoice;
+  }, time);
+
+  return oppChoice;
+}
+
+function startClock() {
+  let clock = setInterval(() => {
+    counter.innerText = count;
+    count--;
+
+    if(count < 0){
+        clearInterval(clock)
+        count = 3
+    }
+  }, time/4);
+
+  
+}
+
+
+function startGame(){
+    
+    startClock()
+    oppChoices();
+}
+startBtn.addEventListener("click", () => {
+    startGame()
+});
+
+
+
+
